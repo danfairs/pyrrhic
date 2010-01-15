7b9db8cd7fd44e7805b98709eac318947866a113
Dan Fairs <dan@fezconsulting.com>
2010-01-15 11:18:22 +0000

    Basic layout from paster

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='pyrrhic',
+      version=version,
+      description="A utility to help test HTTP APIs",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python http rest api',
+      author='Dan Fairs',
+      author_email='dan@fezconsulting.com',
+      url='http://dist.fezconsulting.com/',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
